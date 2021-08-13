f10c5ab845af2b55a44b7740c29659713e5feb38
Hashem A. Damrah <dermdorm2@gmail.com>
2021-08-13 05:24:01 -0700

    Updated plot.py to take arguments like TheCollatzConjecture.py.

--- plot.py
+++ plot.py
@@ -1,3 +1,5 @@
+# 420572
+
 import os
 import argparse
 import pandas as pd
@@ -5,20 +7,35 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    for filename in os.listdir('data'):
-        f = os.path.join('data', filename)
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--start', help='Enter the starting digit')
+
+    args = parser.parse_args()
+
+    if args.start != None:
+        filename = int(args.start)
+    else:
+        filename = 1
+
+    while True:
+        f = os.path.join('data', str(filename) + '.csv')
         if os.path.isfile(f):
             data = pd.read_csv(f)
             x = data['x_value']
             y = data['y_value']
 
             plt.plot(x, y)
-            plt.title('Number: {}'.format(filename[:-4]))
+            plt.title('Number: {}'.format(str(filename)))
 
             plt.show()
 
             x = 0
             y = 0
+
+            filename += 1
+        else:
+            break
 
 if __name__ == '__main__':
     main()
